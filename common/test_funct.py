3d742d760f30af974f582d991b08184cb5230d7a
sutanry03 <sutanry03@gmail.com>
2026-08-05 21:38:20 +0900

    add_test

---
+++ common/test_funct.py
@@ -0,0 +1,13 @@
+from test_script import add, multiply
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+
+def test_multiply():
+    assert multiply(1, 2) == 2
+    assert multiply(-1, 1) == -1
+    assert multiply(-1, -1) == 1
+
+
