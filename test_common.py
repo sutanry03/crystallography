3edadca31de64b6cc6ccb0c868e3beab9476dbc7
sutanry03 <sutanry03@gmail.com>
2026-08-11 18:12:47 +0900

    simplify_matrix_calc

--- test_common.py
+++ test_common.py
@@ -1,6 +1,6 @@
 import compileall
 import pathlib
-from common.lattice_base import Vect, LatticeBasis as LB
+from common.matrix import inverse
 
 def test_library_syntax():
     target_dir = pathlib.Path("./common")
@@ -8,12 +8,10 @@ def test_library_syntax():
     assert is_valid
 
 def test_double_inverse_give_back_itself():
-    sample = [
-        Vect(v=(1., 2., 3.)),
-        Vect(v=(1., 1., 0.)),
-        Vect(v=(1., 1., 1.)),
-    ]
-    bas = LB(basis=tuple(sample))
-    inv = bas.inverse(dim=3)
-    assert bas == inv.inverse(dim=3)
-
+    sample = (
+        (1., 2., 3.),
+        (1., 1., 0.),
+        (1., 1., 1.),
+    )
+    inv = inverse(sample)
+    assert sample == inverse(inv)
